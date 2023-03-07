ed9863c617b390cceca9a151a8749a4c85e17379
Allan Santos <developer.allansantos@outlook.com>
2023-03-06 21:09:31 -0300

    Validação de formulario eventos-detalhe components

--- src/app/components/eventos/eventos-detalhe/eventos-detalhe.component.ts
+++ src/app/components/eventos/eventos-detalhe/eventos-detalhe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,16 +6,24 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './eventos-detalhe.component.html',
   styleUrls: ['./eventos-detalhe.component.css']
 })
-export class EventosDetalheComponent {
+export class EventosDetalheComponent implements OnInit {
   form!: FormGroup;
-  constructor(){}
-  ngOnInit(): void{
 
+  get f(): any{
+    return this.form.controls;
+  }
+
+  constructor(){
+
+
+  }
+  ngOnInit(): void{
+    this.validator();
   }
 
   validator(): void{
     this.form = new FormGroup({
-      tema: new FormControl('', Validators.required),
+      tema: new FormControl('', [Validators.required, Validators.minLength(4)]),
       local: new FormControl('', Validators.required),
       dataEvento: new FormControl('', Validators.required),
       qtdPessoas: new FormControl('', Validators.required),
