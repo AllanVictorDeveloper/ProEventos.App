fe0c0d3b6174a1819881a26b433886da18dd58f8
Allan Santos <developer.allansantos@outlook.com>
2023-03-08 21:12:39 -0300

    Validação formulario registrar

--- src/app/components/user/registration/registration.component.ts
+++ src/app/components/user/registration/registration.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor(public fb: FormBuilder){ }
+
+  get f(): any{
+    return this.form.controls;
+  }
+
+  ngOnInit(): void {
+    this.validator();
+  }
+
+  private validator(): void {
+    this.form = this.fb.group({
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      usuario: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      confirmeSenha: ['', Validators.required],
+    })
+  }
 
 }
